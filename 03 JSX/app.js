971f1a761c34a430475fc76ab0396e8a469d1255
jitender singh <jeetusinghkanyal@gmail.com>
2023-01-19 00:16:53 +0530

    functional component

--- 03 JSX/app.js
+++ 03 JSX/app.js
@@ -1,7 +1,7 @@
 
 import React from "React";
 
-import ReactDOM  from "react-dom";
+import ReactDOM  from "react-dom/client";
 
 
 // created a heading which is nested over h1 and h2 tag
@@ -61,16 +61,17 @@ root.render(container);
 
 
 
-//********************** Component ************************** *//
+//********************** Functional Component ************************** *//
 
 
-const heading=(
-    
-    <h1>Hello world from heading-1</h1>
-);
+const Heading=()=>{
+    return(
+        <h1>Hello world from heading-1</h1>
+    )
+}
 
-const heading2=(
-    <h1>Hello world from heading2</h1>
+const Heading2=()=>(
+    <h1>Hello world from headding-2</h1>
 )
 
 const container=React.createElement(
@@ -78,7 +79,7 @@ const container=React.createElement(
     {
         id:"cont"
     },
-    [heading,heading2]
+    [<Heading />,<Heading2 />]
 )
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
