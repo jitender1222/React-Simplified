fcc451e0733a7a8a027945f0cf7edaa6d34f05c2
jitender singh <jeetusinghkanyal@gmail.com>
2023-01-19 00:01:29 +0530

    heading created using JSX

--- 03 JSX/app.js
+++ 03 JSX/app.js
@@ -3,6 +3,10 @@ import React from "React";
 
 import ReactDOM  from "react-dom";
 
+
+// created a heading which is nested over h1 and h2 tag
+
+/* 
 const heading=React.createElement(
     "h1",
     {
@@ -29,3 +33,58 @@ const container=React.createElement(
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(container);
+
+*/
+
+//**************************By using JSX  ****************************//
+
+// const heading=(
+    
+//     <h1>Hello world from heading-1</h1>
+// );
+
+// const heading2=(
+//     <h1>Hello world from heading2</h1>
+// )
+
+// const container=React.createElement(
+//     "div",
+//     {
+//         id:"cont"
+//     },
+//     [heading,heading2]
+// )
+
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(container);
+
+
+
+//********************** Component ************************** *//
+
+
+const heading=(
+    
+    <h1>Hello world from heading-1</h1>
+);
+
+const heading2=(
+    <h1>Hello world from heading2</h1>
+)
+
+const container=React.createElement(
+    "div",
+    {
+        id:"cont"
+    },
+    [heading,heading2]
+)
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
+
+
+
+
