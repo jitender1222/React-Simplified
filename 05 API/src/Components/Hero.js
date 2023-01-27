ce35984a1313fbb18b9fe5ecab5af094bec7ba06
jitender singh <jeetusinghkanyal@gmail.com>
2023-01-27 22:51:08 +0530

    added search functionality

--- 05 API/src/Components/Hero.js
+++ 05 API/src/Components/Hero.js
@@ -4,42 +4,59 @@ import Card from "./Card";
 
 import { swiggy_api_URL } from "../../config";
 
+import Shimmer from "../Components/Shimmer";
 
-export default Hero = () => {
-  const [data, setData] = useState([]);
-
-  const fetchData = async () => {
-    try {
-      const req = await fetch(swiggy_api_URL);
-
-      const json = await req.json();
-
-      let data = json?.data?.cards[2]?.data?.data?.cards;
-
-      setData(data);
-    } catch (error) {
-      console.log("this is ans " + error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return (
-    <>
-      <div className="hero">
-        <div className="search">
-          <input type="text" placeholder="Enter your text"></input>
-        </div>
-
-        <div className="resCard">
-          {data.map((datas) => {
-            console.log(datas)
-            return <Card {...datas.data} key={datas.data.id} />;
-          })}
-        </div>
-      </div>
-    </>
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
   );
-};
+  return filterData;
+}
+
+
+const Hero=()=>{
+
+  const [restraunt,setRestraunt]=useState([]);
+
+  const [filterText,setFilterText]=useState([]);
+
+  const [searchText,setSearchText]=useState("");
+
+
+  const fetchData=async ()=>{
+    
+    const data=await fetch(swiggy_api_URL);
+    const json=await data.json();
+    console.log(json);
+    setRestraunt(json?.data?.cards[2]?.data?.data?.cards);
+    setFilterText(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+  return restraunt?.length===0? (<Shimmer />):(
+    <>
+    <div className="input-type">
+      <input type="text" placeholder="Search"
+      onChange={(e)=>{(
+        setSearchText(e.target.value)
+      )}}
+      ></input>
+      <button onClick={()=>{
+        const data=filterData(searchText,restraunt);
+        setFilterText(data);
+      }}>Search</button>
+    </div>
+    <div className="restaurant-list">
+      {
+        filterText.map((item)=>{
+          return <Card {...item.data} key={item.data.id} />
+        })
+      }
+    </div>
+    </>
+  )
+}
+
+export default Hero;
