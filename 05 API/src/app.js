4f6b12cd45150591cf307554e6b0600a9431c6a6
jitender singh <jeetusinghkanyal@gmail.com>
2023-01-28 14:47:21 +0530

    added router

--- 05 API/src/app.js
+++ 05 API/src/app.js
@@ -5,6 +5,10 @@ import  ReactDOM from "react-dom/client";
 import Header from "./Components/Header"
 import Hero from "./Components/Hero";
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./Components/About";
+import Error from "./Components/Error";
+
 const App=()=>{
     return(
         <>
@@ -14,6 +18,22 @@ const App=()=>{
     )
 }
 
+const AppRouter=createBrowserRouter([
+
+    {
+        path:"/",
+        element:<App />,
+        errorElement:<Error />
+    },
+
+    {
+        path:"/about",
+        element:<About />
+    }
+
+
+])
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<RouterProvider router={AppRouter} />);
